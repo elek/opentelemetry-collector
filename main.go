bec5e07786473aa1874aed86c114daec28ad46b8
Márton Elek <elek@apache.org>
2026-02-17 17:47:37 +0100

    manifest: bump version

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	info := component.BuildInfo{
 		Command:     "otelcol",
 		Description: "OpenTelemetry Collector. Custom distribution",
-		Version:     "1.0.0",
+		Version:     "1.2.2",
 	}
 
 	set := otelcol.CollectorSettings{
